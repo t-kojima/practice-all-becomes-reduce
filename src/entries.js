3f98a3e505613df1c3167532af6042e3348fb22a
t-kojima <t-kojima@skullware.net>
2018-07-21 02:59:16 +0900

    fix: entries and test

--- src/entries.js
+++ src/entries.js
@@ -1,19 +1,16 @@
 Array.prototype.entries = function() {
-  // const iterator = this[Symbol.iterator]();
-  const iterator = {};
+  const iterator = [];
   let count = 0;
-  iterator.next = () =>
-    this.reduce((acc, cur, index) => {
-      if (count === index) {
-        acc.push({ value: this[index], done: false });
-        count += 1;
-      }
-      return acc;
-    }, [])[0];
+  iterator.next = () => {
+    try {
+      return this.reduce(
+        (acc, cur, index) =>
+          count === index ? { value: [index, this[index]], done: false } : acc,
+        { value: undefined, done: true }
+      );
+    } finally {
+      count += 1;
+    }
+  };
   return iterator;
 };
-
-// const array = [1, 2, 3, 4, 5]
-// // console.log(array.entries())
-// const ite = array.entries();
-// console.log(ite.next)
--- test/entries_test.js
+++ test/entries_test.js
@@ -2,11 +2,25 @@ require('../src/entries');
 const { assert } = require('chai');
 
 describe('entries', () => {
-  it('do', () => {
+  it('called next() at 7 times', () => {
     const array = [1, 2, 3, 4, 5];
     const iterator = array.entries();
-    assert.equal(iterator.next().value, 1);
-    assert.equal(iterator.next().value, 2);
-    assert.equal(iterator.next().value, 3);
+    assert.deepEqual(iterator.next().value, [0, 1]);
+    assert.deepEqual(iterator.next().value, [1, 2]);
+    assert.deepEqual(iterator.next().value, [2, 3]);
+    assert.deepEqual(iterator.next().value, [3, 4]);
+    assert.deepEqual(iterator.next().value, [4, 5]);
+    assert.equal(iterator.next().value, undefined);
+    assert.equal(iterator.next().value, undefined);
+  });
+
+  it('can called by for...of', () => {
+    const array = [1, 2, 3, 4, 5];
+    const iterator = array.entries();
+    // eslint-disable-next-line no-restricted-syntax
+    for (const i of iterator) {
+      assert.deepEqual(i, [0, 1]);
+      break;
+    }
   });
 });
