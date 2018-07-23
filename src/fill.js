af45301c52b3e1123d3331843e4bb861c741898b
t-kojima <t-kojima@skullware.net>
2018-07-23 21:06:35 +0900

    fix: sparse in fill and test

--- src/fill.js
+++ src/fill.js
@@ -1,13 +1,15 @@
 Array.prototype.fill = function(target, start = 0, end = this.length) {
-  const parse = value => {
-    if (value > this.length) return this.length;
-    if (value + this.length < 0) return 0;
-    return Number.parseInt(value, 10) + (value < 0 ? this.length : 0);
-  };
+  const list = array => Array.from(Array(array.length));
+  const parse = value =>
+    Math.min(
+      Math.max(Number.parseInt(value, 10) + (value < 0 ? this.length : 0), 0),
+      this.length
+    );
+
   const s = parse(start);
   const e = parse(end);
 
-  return this.reduce((acc, cur, index) => {
+  return list(this).reduce((acc, cur, index) => {
     if (index >= s && index < e) {
       acc[index] = target;
     }
--- test/fill_test.js
+++ test/fill_test.js
@@ -2,6 +2,14 @@ require('../src/fill');
 const { assert } = require('chai');
 
 describe('fill', () => {
+  describe('0 args', () => {
+    it('return all undefined', () => {
+      const array = [1, 2, 3, 4];
+      const actual = array.fill();
+      assert.deepEqual(actual, [undefined, undefined, undefined, undefined]);
+    });
+  });
+
   describe('1 args (target only)', () => {
     it('return all 0', () => {
       const array = [1, 2, 3, 4];
@@ -25,6 +33,22 @@ describe('fill', () => {
       const array = [1, 2, 3, 4];
       array.fill(0);
       assert.deepEqual(array, [0, 0, 0, 0]);
+    });
+
+    describe('empty array', () => {
+      it('return empty', () => {
+        const array = [];
+        const actual = array.fill('A');
+        assert.deepEqual(actual, []);
+      });
+    });
+
+    describe('sparse array', () => {
+      it('return all 0', () => {
+        const array = [, , ,];
+        const actual = array.fill(0);
+        assert.deepEqual(actual, [0, 0, 0]);
+      });
     });
   });
 
@@ -57,6 +81,22 @@ describe('fill', () => {
       const array = [1, 2, 3, 4, 5, 6, 7, 8];
       const actual = array.fill(0, -9);
       assert.deepEqual(actual, [0, 0, 0, 0, 0, 0, 0, 0]);
+    });
+
+    describe('empty array', () => {
+      it('return empty starts with 2', () => {
+        const array = [];
+        const actual = array.fill('A', 2);
+        assert.deepEqual(actual, []);
+      });
+    });
+
+    describe('sparse array', () => {
+      it('fill 0 starts with 2', () => {
+        const array = [, , ,];
+        const actual = array.fill(0, 2);
+        assert.deepEqual(actual, [, , 0]);
+      });
     });
   });
 
@@ -95,6 +135,22 @@ describe('fill', () => {
       const array = [1, 2, 3, 4, 5, 6, 7, 8];
       const actual = array.fill(0, 0, -9);
       assert.deepEqual(actual, [1, 2, 3, 4, 5, 6, 7, 8]);
+    });
+
+    describe('empty array', () => {
+      it('return empty at 2 to -3', () => {
+        const array = [];
+        const actual = array.fill('A', 2, -3);
+        assert.deepEqual(actual, []);
+      });
+    });
+
+    describe('sparse array', () => {
+      it('fill 0 at 1 to 3', () => {
+        const array = Array(10);
+        const actual = array.fill(0, 1, 3);
+        assert.deepEqual(actual, [, 0, 0, , , , , , , ,]);
+      });
     });
   });
 });
