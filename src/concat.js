a04c2cdc779e2898dd7e1057277e2108b2c7e56d
t-kojima <t-kojima@skullware.net>
2018-07-23 21:02:59 +0900

    fix: sparse in concat and test

--- src/concat.js
+++ src/concat.js
@@ -1,9 +1,31 @@
-/* eslint-disable array-callback-return */
+/* eslint-disable array-callback-return, no-param-reassign */
 Array.prototype.concat = function(...args) {
-  const copy = Array.from(this);
-  args.reduce((_, array) => {
-    array.reduce((acc, cur) => {
-      copy[copy.length] = cur;
+  const list = array => Array.from(Array(array.length));
+  const pushValue = (from, to, index) => {
+    if (index in from) {
+      to[to.length] = from[index];
+    } else {
+      to.length += 1;
+    }
+  };
+
+  // thisを破壊しない為、コピー配列に結果を入れていく。
+  // 疎要素のコピーとreduce of empty array回避の為、
+  // 同じ長さの配列を用意してreduceしてコピーする。
+  const copy = list(this).reduce((acc, cur, index) => {
+    pushValue(this, acc, index);
+    return acc;
+  }, []);
+
+  // 引数に値がある場合は配列に変換する。
+  const arrays = args.reduce((acc, cur) => {
+    acc[acc.length] = Array.isArray(cur) ? cur : [cur];
+    return acc;
+  }, []);
+
+  arrays.reduce((_, array) => {
+    list(array).reduce((acc, cur, index) => {
+      pushValue(array, copy, index);
     }, null);
   }, null);
   return copy;
--- test/concat_test.js
+++ test/concat_test.js
@@ -25,4 +25,91 @@ describe('concat', () => {
     array.concat([5, 6, 7, 8]);
     assert.deepEqual(array, [1, 2, 3, 4]);
   });
+
+  describe('concat value', () => {
+    it('concat number', () => {
+      const array = [1, 2];
+      const actual = array.concat(3);
+      assert.deepEqual(actual, [1, 2, 3]);
+    });
+
+    it('concat number and array', () => {
+      const array = [1, 2];
+      const actual = array.concat(3, [4, 5]);
+      assert.deepEqual(actual, [1, 2, 3, 4, 5]);
+    });
+  });
+
+  describe('concat nested array', () => {
+    it('nested array', () => {
+      const array = [[1], 2];
+      const actual = array.concat([[3]]);
+      assert.deepEqual(actual, [[1], 2, [3]]);
+    });
+  });
+
+  describe('concat array with shallow copy', () => {
+    it('shallow copy', () => {
+      const array = [{ a: 1 }, { b: 2 }];
+      const actual = array.concat([{ c: 3 }]);
+      assert.deepEqual(actual, [{ a: 1 }, { b: 2 }, { c: 3 }]);
+      array[0].a = 4;
+      assert.deepEqual(actual, [{ a: 4 }, { b: 2 }, { c: 3 }]);
+    });
+  });
+
+  describe('empty array', () => {
+    it('return empty with no args', () => {
+      assert.deepEqual([].concat(), []);
+    });
+
+    it('return empty', () => {
+      assert.deepEqual([].concat([]), []);
+    });
+  });
+
+  describe('sparse array', () => {
+    it('return sparse array when sparse receiver', () => {
+      const actual = Array(2).concat([]);
+      assert.deepEqual(actual, Array(2));
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 0));
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 1));
+    });
+
+    it('return sparse array includes 1', () => {
+      const actual = [, 1, ,].concat([]);
+      assert.deepEqual(actual, [, 1, ,]);
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 0));
+      assert.isTrue(Object.prototype.hasOwnProperty.call(actual, 1));
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 2));
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 3));
+    });
+
+    it('return sparse array when sparse arg', () => {
+      const actual = [].concat(Array(3));
+      assert.deepEqual(actual, Array(3));
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 0));
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 1));
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 2));
+    });
+
+    it('return sparse array when sparse receiver and arg', () => {
+      const actual = Array(2).concat(Array(3));
+      assert.deepEqual(actual, Array(5));
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 0));
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 1));
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 2));
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 3));
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 4));
+    });
+
+    it('return aparse array includes undefined', () => {
+      const actual = [undefined].concat(Array(2), [undefined]);
+      assert.deepEqual(actual, [undefined, , , undefined]);
+      assert.isTrue(Object.prototype.hasOwnProperty.call(actual, 0));
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 1));
+      assert.isFalse(Object.prototype.hasOwnProperty.call(actual, 2));
+      assert.isTrue(Object.prototype.hasOwnProperty.call(actual, 3));
+    });
+  });
 });
