79822a147cf18b610b211e98d4fa2d2fa2a9f5d9
t-kojima <t-kojima@skullware.net>
2018-07-23 21:04:09 +0900

    fix: sparse in copyWithin and test

--- src/copy_within.js
+++ src/copy_within.js
@@ -1,20 +1,48 @@
+/* eslint-disable no-param-reassign */
+
+// 長さが同じで要素がundefinedな配列を返す
+// 疎の配列の場合reduce of empty array回避の為
+const list = array => Array.from(Array(array.length));
+
+// 配列(to)へ配列(from)のindex位置の要素をpushする。
+// index位置が疎要素の場合はlengthを拡張して対応
+// 疎要素を維持したままpushできる。
+const pushValue = (from, to, index) => {
+  if (index in from) {
+    to[to.length] = from[index];
+  } else {
+    to.length += 1;
+  }
+};
+
+// 配列をコピーする。array.concat()と等価
+// array#concatが利用できないのでreduceで実装
+const clone = array =>
+  list(array).reduce((acc, cur, index) => {
+    pushValue(array, acc, index);
+    return acc;
+  }, []);
+
 Array.prototype.copyWithin = function(target, start = 0, end = this.length) {
-  const copy = Array.from(this);
-  const parse = value => {
-    if (value > this.length) return this.length;
-    if (value + this.length < 0) return 0;
-    return value < 0
-      ? this.length + Number.parseInt(value, 10)
-      : Number.parseInt(value, 10);
-  };
+  const parse = value =>
+    Math.min(
+      Math.max(Number.parseInt(value, 10) + (value < 0 ? this.length : 0), 0),
+      this.length
+    );
+
   const t = parse(target);
   const s = parse(start);
   const e = parse(end);
 
-  return this.reduce((acc, cur, index) => {
-    if (index >= t && index < e - s + t) {
-      acc[index] = copy[s + index - t];
-    }
+  const copy = clone(this);
+
+  this.length = 0;
+  return list(copy).reduce((acc, cur, index) => {
+    pushValue(
+      copy,
+      acc,
+      index >= t && index < e - s + t ? s + index - t : index
+    );
     return acc;
   }, this);
 };
--- test/copy_within_test.js
+++ test/copy_within_test.js
@@ -9,6 +9,12 @@ describe('copyWithin', () => {
       assert.deepEqual(actual, [1, 2, 3, 4, 5, 6, 7, 8]);
     });
 
+    it('return same when not args', () => {
+      const array = [1, 2, 3, 4, 5, 6, 7, 8];
+      const actual = array.copyWithin();
+      assert.deepEqual(actual, [1, 2, 3, 4, 5, 6, 7, 8]);
+    });
+
     it('shift 1', () => {
       const array = [1, 2, 3, 4, 5, 6, 7, 8];
       const actual = array.copyWithin(1);
@@ -43,6 +49,44 @@ describe('copyWithin', () => {
       const array = [1, 2, 3, 4];
       const actual = array.copyWithin(-6);
       assert.deepEqual(actual, [1, 2, 3, 4]);
+    });
+
+    describe('empty array', () => {
+      it('return empty array when shift 0', () => {
+        assert.deepEqual([].copyWithin(0), []);
+      });
+
+      it('return empty array when shift 1', () => {
+        assert.deepEqual([].copyWithin(1), []);
+      });
+
+      it('return empty array when shift -1', () => {
+        assert.deepEqual([].copyWithin(-1), []);
+      });
+    });
+
+    describe('sparse array', () => {
+      it('shift 1', () => {
+        const array = [, , 2, , 4];
+        const actual = array.copyWithin(1);
+        assert.deepEqual(actual, [, , , 2, ,]);
+        assert.isFalse(0 in actual);
+        assert.isFalse(1 in actual);
+        assert.isFalse(2 in actual);
+        assert.isTrue(3 in actual);
+        assert.isFalse(4 in actual);
+      });
+
+      it('shift -2', () => {
+        const array = [, , 2, , 4];
+        const actual = array.copyWithin(-2);
+        assert.deepEqual(actual, [, , 2, , ,]);
+        assert.isFalse(0 in actual);
+        assert.isFalse(1 in actual);
+        assert.isTrue(2 in actual);
+        assert.isFalse(3 in actual);
+        assert.isFalse(4 in actual);
+      });
     });
   });
 
@@ -82,6 +126,40 @@ describe('copyWithin', () => {
       const actual = array.copyWithin(4, -9);
       assert.deepEqual(actual, [1, 2, 3, 4, 1, 2, 3, 4]);
     });
+
+    describe('empty array', () => {
+      it('return empty array when shift 0 starts with 2', () => {
+        assert.deepEqual([].copyWithin(0, 2), []);
+      });
+
+      it('return empty array when shift 1 starts with -2', () => {
+        assert.deepEqual([].copyWithin(1, -2), []);
+      });
+    });
+
+    describe('sparse array', () => {
+      it('shift 1 starts with 2', () => {
+        const array = [, , 2, , 4];
+        const actual = array.copyWithin(1, 2);
+        assert.deepEqual(actual, [, 2, , 4, 4]);
+        assert.isFalse(0 in actual);
+        assert.isTrue(1 in actual);
+        assert.isFalse(2 in actual);
+        assert.isTrue(3 in actual);
+        assert.isTrue(4 in actual);
+      });
+
+      it('shift -2 starts with -1', () => {
+        const array = [, , 2, , 4];
+        const actual = array.copyWithin(-2, -1);
+        assert.deepEqual(actual, [, , 2, 4, 4]);
+        assert.isFalse(0 in actual);
+        assert.isFalse(1 in actual);
+        assert.isTrue(2 in actual);
+        assert.isTrue(3 in actual);
+        assert.isTrue(4 in actual);
+      });
+    });
   });
 
   describe('3 args (target and start and end)', () => {
@@ -119,6 +197,40 @@ describe('copyWithin', () => {
       const array = [1, 2, 3, 4, 5, 6, 7, 8];
       const actual = array.copyWithin(4, 0, -9);
       assert.deepEqual(actual, [1, 2, 3, 4, 5, 6, 7, 8]);
+    });
+
+    describe('empty array', () => {
+      it('empty array when shift 0 starts with 2 at 1 value', () => {
+        assert.deepEqual([].copyWithin(0, 2, 1), []);
+      });
+
+      it('empty array when shift 1 starts with 2 at -1 value', () => {
+        assert.deepEqual([].copyWithin(1, 2, -1), []);
+      });
+    });
+
+    describe('sparse array', () => {
+      it('shift 1 starts with 2 at 2 value', () => {
+        const array = [, , 2, , 4];
+        const actual = array.copyWithin(1, 2, 4);
+        assert.deepEqual(actual, [, 2, , , 4]);
+        assert.isFalse(0 in actual);
+        assert.isTrue(1 in actual);
+        assert.isFalse(2 in actual);
+        assert.isFalse(3 in actual);
+        assert.isTrue(4 in actual);
+      });
+
+      it('shift 2 starts with -2 at -1 value', () => {
+        const array = [, , 2, , 4];
+        const actual = array.copyWithin(2, -2, -1);
+        assert.deepEqual(actual, [, , , , 4]);
+        assert.isFalse(0 in actual);
+        assert.isFalse(1 in actual);
+        assert.isFalse(2 in actual);
+        assert.isFalse(3 in actual);
+        assert.isTrue(4 in actual);
+      });
     });
   });
 });
